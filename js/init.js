4ec28dc0f66e093616a981771f3adad20c78b77d
whats0n <mrdvv@mail.ru>
2014-12-29 18:16:56 +0200

    fixed

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@ head.load("js/lib/jquery.js",
 					"js/lib/jquery-ui.min.js",
 					"js/lib/slick.min.js",
 					"js/lib/cycle.js",
+					"js/lib/cycle2.carousel.min.js",
 					"js/lib/cycle2.swipe.js",
 					"js/lib/jquery.touchSwipe.min.js",
 					"js/common.js");
